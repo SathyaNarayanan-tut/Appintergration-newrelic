bbf58f45ffc3b6d89da38743977a653e3e1dc28b
SathyaNarayanan-tut <60778744+SathyaNarayanan-tut@users.noreply.github.com>
2025-02-06 13:17:32 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,12 +22,3 @@ variable "newrelic_ini_path" {
 description = "Path to the New Relic configuration file"
 type = string
 }
-Summary: The `variables.tf` file defines input variables for reusability and customization,
-including credentials and file paths.
-File - terraform.tfvars
-key_name = "Test"
-app_file = "/Users/sathyanarayanan/Desktop/Assignment-web/app.py"
-private_key_path = "/Users/sathyanarayanan/Desktop/Test.pem"
-new_relic_license_key = "eu01xxbbb98af6d0b9065449b3d3de11XXXXXX"
-new_relic_account_id = "10065218XX"
-newrelic_ini_path = "/Users/sathyanarayanan/Desktop/newrelic-1.ini"
